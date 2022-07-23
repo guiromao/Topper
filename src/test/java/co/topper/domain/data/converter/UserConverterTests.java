c5f872e1d4b84355f55d3ac3b1a396811a93364d
Guilherme Amoroso Romão <gromao@primeit.pt>
2022-07-23 10:31:47 +0100

    Adding UserConverter tests

---
+++ src/test/java/co/topper/domain/data/converter/UserConverterTests.java
@@ -0,0 +1,70 @@
+package co.topper.domain.data.converter;
+
+import co.topper.domain.data.dto.UserDto;
+import co.topper.domain.data.entity.Role;
+import co.topper.domain.data.entity.UserEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.Set;
+
+@SpringBootTest
+class UserConverterTests {
+
+    private static final Instant NOW = Instant.now();
+
+    @Autowired
+    UserConverter userConverter;
+
+    @Test
+    void testConvertUserLastVoteToday() {
+        UserEntity userVotedToday = userWithLastVoteAt(NOW);
+
+        UserDto test = userConverter.toDto(userVotedToday);
+
+        Assertions.assertEquals("user-1", test.getUserId());
+        Assertions.assertEquals("username", test.getUsername());
+        Assertions.assertNull(test.getPassword());
+        Assertions.assertEquals("username@mail.com", test.getEmail());
+        Assertions.assertEquals(Map.of("artist-1", 800L), test.getTrackVotes());
+        Assertions.assertEquals(1000L, test.getAvailableVotes());
+        Assertions.assertEquals(NOW, test.getLastVoteAttempt());
+    }
+
+    @Test
+    void testConvertUserLastVoteBeforeToday() {
+        Instant beforeDate = NOW.minus(2, ChronoUnit.DAYS);
+        UserEntity user = userWithLastVoteAt(beforeDate);
+
+        UserDto test = userConverter.toDto(user);
+
+        Assertions.assertEquals("user-1", test.getUserId());
+        Assertions.assertEquals("username", test.getUsername());
+        Assertions.assertNull(test.getPassword());
+        Assertions.assertEquals("username@mail.com", test.getEmail());
+        Assertions.assertEquals(Map.of("artist-1", 800L), test.getTrackVotes());
+        Assertions.assertEquals(3000L, test.getAvailableVotes());
+        Assertions.assertEquals(beforeDate, test.getLastVoteAttempt());
+    }
+
+    private UserEntity userWithLastVoteAt(Instant instant) {
+        return new UserEntity(
+            "user-1",
+            "username",
+            "pass",
+            "username@mail.com",
+            Set.of("friendId"),
+            Set.of("requestId-1", "requestId-2"),
+            Map.of("artist-1", 800L),
+            1000L,
+            instant,
+            Set.of(Role.USER)
+        );
+    }
+
+}
